bbe7769122a49c8152d57520b401fe9d5dd7d2b2
mitch <6061049+mitchharvey@users.noreply.github.com>
2023-08-23 13:43:01 -0400

    create repos if not exist

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Generate ssh keys if not exist
+ssh-keygen -A
+
 # If there is some public key in keys folder
 # then it copies its contain in authorized_keys file
 if [ "$(ls -A /git-server/keys/)" ]; then
